6b9075371a6ae61e338d73741c8580a388082afd
SolubleApollo <SolubleApollo@gmail.com>
2021-07-27 13:39:45 -0500

    init
    Issues

---
+++ RocketReserver/RocketReserver/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RocketReserver
+//
+//  Created by Marqet Group on 7/27/21.
+//
+
+import UIKit
+var launches = [LaunchListQuery.Data.Launch.Launch]()
+
+enum ListSection: Int, CaseIterable {
+  case launches
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
